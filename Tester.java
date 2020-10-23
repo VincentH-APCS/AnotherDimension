1b45def9984a01a49419bb658571ad472671fa6d
VincentH-APCS <vhuang10@stuy.edu>
2020-10-23 13:17:12 -0400

    Tested array of {3,3,11,4,5}

--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[] args){
     int[] arr = {3,3,11,4,5};
-    System.out.println(ArrayOps.sum(arr));
+  //System.out.println(ArrayOps.sum(arr));
+    System.out.println(ArrayOps.largest(arr));
   }
 }

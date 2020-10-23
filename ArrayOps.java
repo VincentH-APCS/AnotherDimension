8a00abae4c562ae0cedd864b9931e92eacbc4ba7
VincentH-APCS <vhuang10@stuy.edu>
2020-10-23 13:08:21 -0400

    wrote first attempt of sum

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,3 +1,9 @@
 public class ArrayOps{
-  
+  public static int sum(int[] arr){
+    int thesum = 0;
+    for (int i = 0; i < arr.length; i++){
+      thesum = thesum + arr[i];
+    }
+    return thesum;
+  }
 }

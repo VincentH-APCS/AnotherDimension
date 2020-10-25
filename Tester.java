ac06cb10efe681ede96d9dfadf198215397742a2
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:01:27 -0400

    Tested with negative numbers only in largest

--- Tester.java
+++ Tester.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{1,3,3,3,3,3},{1122,5},{6,7},{0}};
+    int[] arr = {-6,-7,-8,-1};
   //System.out.println(ArrayOps.sum(arr));
-  //System.out.println(ArrayOps.largest(arr));
+  System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
   //System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
-  System.out.println(ArrayOps.sum(arr));
+  //System.out.println(ArrayOps.sum(arr));
   }
 }

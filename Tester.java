3f9ff1aad8569d03180df9f709176defb2b0fb25
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:43:08 -0400

    Tested for different sum of rows, but same sum of columns

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{1,2,3},{3,2,1},{2,2,2},{2,2,2}};
+    int[][] arr = {{7,8,15},{1,1,1},{19,11,4},{3,10,10}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));

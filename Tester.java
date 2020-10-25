89715fbc8b619c4ee9d356b8b521b56a8926d3cc
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 21:14:22 -0400

    Second test of sumRows

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{1},{3,3,3},{2,5}};
+    int[][] arr = {{16,16},{1008,0,0,0,1},{43,1,111}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));

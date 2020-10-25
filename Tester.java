7d69b4601704c87a84e2feac85aed1ccd1582eb6
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:52:39 -0400

    tested updated largestInRows

--- Tester.java
+++ Tester.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{22,2,6},{14,1,5},{19,12,-55},{-10,10,15}};
+  int[][] arr = {{22,2,6},{14,1,5},{19,12,-55},{-10,10,15}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
-  //System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
+  System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumCols(arr))));
-  System.out.println(ArrayOps.isColMagic(arr));
+  //System.out.println(ArrayOps.isColMagic(arr));
   }
 }

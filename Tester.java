b5b10119b09d0d63fb8dac7c9f72d9ea4325d6aa
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:46:48 -0400

    one last test for isColMagic

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{9,7,6},{12,1,5},{19,12,4},{-10,10,15}};
+    int[][] arr = {{22,2,6},{14,1,5},{19,12,-55},{-10,10,15}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));

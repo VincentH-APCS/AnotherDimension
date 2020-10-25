f8fcfe6e120c3bfec0b9c06433857b496e3af0a1
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 23:00:35 -0400

    Tested isLocationMagic--success

--- Tester.java
+++ Tester.java
@@ -1,13 +1,14 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-  int[][] arr = {{22,2,6},{14,1,5},{19,12,-55},{-10,10,15}};
+  int[][] arr = {{0,12,6},{7,6,5,66}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
-  System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
+  //System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumCols(arr))));
   //System.out.println(ArrayOps.isColMagic(arr));
+  System.out.println(ArrayOps.isLocationMagic(arr, 0, 1));
   }
 }

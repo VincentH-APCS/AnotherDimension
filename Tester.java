5ba1b7d0cd957a57c0ba85ef3139f46372751bba
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 21:34:31 -0400

    Third test of largestInRows

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{5,6,6},{16,333,334,333},{61,43,7},{0}};
+    int[][] arr = {{0,41,0},{22,22,22,22},{86,55},{0,0,4}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));

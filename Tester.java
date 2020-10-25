71b699d3634c57340f65d97ec296ccfce5559530
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 21:51:54 -0400

    More tests of new sum

--- Tester.java
+++ Tester.java
@@ -1,10 +1,11 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{0,41,0},{22,22,22,22},{86,55},{0,0,4}};
+    int[][] arr = {{12,12,12},{12,12},{12},{0}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
-  System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
+  //System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
+  System.out.println(ArrayOps.sum(arr));
   }
 }

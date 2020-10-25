a3994a1bdaba2f80e7dbbd27e16206d4a7500d28
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:23:44 -0400

    First Test of sumCols

--- Tester.java
+++ Tester.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{-6,-7,-8,-1},{-5,-4,-2},{-13}};
+    int[][] arr = {{4,12,5},{7,7,7},{123,0,14},{3,2,1}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
-  System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
+  //System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
   //System.out.println(ArrayOps.sum(arr));
+  System.out.println(Arrays.toString((ArrayOps.sumCols(arr))));
   }
 }

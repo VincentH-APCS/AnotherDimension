c07f006519200bf52122ad2688574df555a80796
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:46:17 -0400

    MORE TESTS FOR ISCOLMAGIC

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{9,1,6},{12,1,5},{19,11,4},{-10,10,15}};
+    int[][] arr = {{9,7,6},{12,1,5},{19,12,4},{-10,10,15}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));

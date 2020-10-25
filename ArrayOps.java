d90a64729b401f9ce32266ffb5b9c8c20d927a4c
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:01:09 -0400

    Modified Largest Methods in case the array is entirely negative numbers

--- ArrayOps.java
+++ ArrayOps.java
@@ -8,6 +8,9 @@ public class ArrayOps{
   }
   public static int largest(int[] arr){
     int thelargest = 0;
+    if (arr[0] < 0){
+      thelargest = arr[0];
+    }
     for(int i = 0; i < arr.length; i++){
       if (arr[i] > thelargest){
         thelargest = arr[i];
@@ -25,6 +28,9 @@ public class ArrayOps{
   public static int[] largestInRows(int[][] matrix){
     int[] outcome = new int[matrix.length];
     int largest = 0;
+    if (matrix[0][0] < 0){
+      largest = matrix[0][0];
+    }
     for (int i = 0; i < matrix.length; i++){
       for(int j = 0; j < matrix[i].length; j++){
         if (matrix[i][j] > largest){
@@ -40,4 +46,7 @@ public class ArrayOps{
     int[] sums = ArrayOps.sumRows(arr);
     return ArrayOps.sum(sums);
   }
+  //public static int[] sumCols(int[][] matrix){
+
+//  }
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{12,12,12},{12,12},{12},{0}};
+    int[][] arr = {{1,3,3,3,3,3},{1122,5},{6,7},{0}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));

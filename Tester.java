dcd7ea8ae3140ea8f33a925ccac52d82f8e96f19
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:31:55 -0400

    First test of isRowMagic method

--- Tester.java
+++ Tester.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{4,12,5},{7,7,7},{123,0,14},{3,2,1}};
+    int[][] arr = {{5,5,5},{5,5,5},{5,5,5},{5,5,5}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
   //System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
   //System.out.println(ArrayOps.sum(arr));
-  System.out.println(Arrays.toString((ArrayOps.sumCols(arr))));
+  //System.out.println(Arrays.toString((ArrayOps.sumCols(arr))));
+  System.out.println(ArrayOps.isRowMagic(arr));
   }
 }

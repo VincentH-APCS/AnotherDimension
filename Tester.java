5b27476902cfc57b50fe51a88664ee22d66a7b6f
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:04:57 -0400

    tested negative only largestInRows

--- Tester.java
+++ Tester.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[] arr = {-6,-7,-8,-1};
+    int[][] arr = {{-6,-7,-8,-1},{-5,-4,-2},{-13}};
   //System.out.println(ArrayOps.sum(arr));
-  System.out.println(ArrayOps.largest(arr));
+  //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
-  //System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
+  System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
   //System.out.println(ArrayOps.sum(arr));
   }
 }

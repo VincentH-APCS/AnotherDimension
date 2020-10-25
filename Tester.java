ddd50189c1a3b7a0d09b0e2678958043707dae5f
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 22:41:43 -0400

    The first test of isColMagic

--- Tester.java
+++ Tester.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{5},{0},{7},{5}};
+    int[][] arr = {{1,2,3},{3,2,1},{2,2,2},{2,2,2}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
   //System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(Arrays.toString((ArrayOps.sumCols(arr))));
-  System.out.println(ArrayOps.isRowMagic(arr));
+  System.out.println(ArrayOps.isColMagic(arr));
   }
 }

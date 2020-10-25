e9c53ca15dd4d51c92740efa27ac716daf315e56
VincentH-APCS <vhuang10@stuy.edu>
2020-10-24 21:25:11 -0400

    first test of largestInRows

--- Tester.java
+++ Tester.java
@@ -1,9 +1,10 @@
 import java.util.Arrays;
 public class Tester{
   public static void main(String[] args){
-    int[][] arr = {{16,16},{1008,0,0,0,1},{43,1,111}};
+    int[][] arr = {{5},{6,6,3,12},{5000,3,333}};
   //System.out.println(ArrayOps.sum(arr));
   //System.out.println(ArrayOps.largest(arr));
-  System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
+  //System.out.println(Arrays.toString((ArrayOps.sumRows(arr))));
+  System.out.println(Arrays.toString((ArrayOps.largestInRows(arr))));
   }
 }
